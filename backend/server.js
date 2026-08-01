337b9b617e998772894be4c82435bc3f42ec582e
Delphine Hayicou <hayoudel@gmail.com>
2026-08-01 18:59:09 +0000

    Ajout du backend learner

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connection } from "./config/db.js";
 import { sequelize } from "./config/db.js";
+import learnerRoutes from "./Routes/learnerRoutes.js"
 
 dotenv.config();
 
@@ -11,13 +12,14 @@ app.use(express.json());
 
 
 const port = process.env.PORT || 7000;
+app.use("/api",learnerRoutes);
 
 
 const connecte = async () => {
     try {
 
         await connection();
-        await sequelize.sync();
+        await sequelize.sync({ alter: true });
 
         app.listen(port, () => {
             console.log(`Serveur connecté sur le port: ${port}`);
@@ -29,9 +31,4 @@ const connecte = async () => {
 
     }
 };
-
-
-
-
-
 connecte();
